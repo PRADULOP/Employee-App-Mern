3d6ada5709481bd030d6088120ce262cbf1b7f1e
PRADUL OP <97777098+PRADULOP@users.noreply.github.com>
2025-01-19 00:36:20 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Login from './components/Login';
 import Main from './components/Main';
 import Addemployee from './components/Addemployee';
 import { Route, Routes } from 'react-router-dom';
-// import PrivateRoutes from './components/PrivateRoutes';
+import PrivateRoutes from './components/PrivateRoutes';
 import AdminPage from './components/AdminPage';
 import UserPage from './components/UserPage';
 
@@ -11,11 +11,11 @@ const App = () => {
   return (
       <Routes>
         <Route path='/' element={<Login />} />
-        {/* <Route element={<PrivateRoutes/>}> */}
+        <Route element={<PrivateRoutes/>}>
           <Route path="/admin" element={<Main child={<AdminPage />} />} />
           <Route path="/user" element={<Main child={<UserPage />} />} />
           <Route path="/addemployee" element={<Main child={<Addemployee />} />} />
-        {/* </Route> */}
+        </Route>
       </Routes>
   );
 };
